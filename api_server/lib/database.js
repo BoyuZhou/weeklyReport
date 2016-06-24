61b2ff82776729139d18ac038f5f450b153a1161
unknown <2470200492@qq.com>
2016-06-24 18:11:52 +0800

    3

--- api_server/lib/database.js
+++ api_server/lib/database.js
@@ -1,3 +1,10 @@
 var waterline = require('waterline');
 
-
+module.exports = waterline.Collection.extend({
+   identity: 'bear',   //对应表名
+    connection: 'nyKocalMysql', //数据库连接
+    schema: true,
+    attributes: {   //存入的数据
+        name: 'String'
+    }
+});
